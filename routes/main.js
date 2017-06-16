c5f83769209940a916e884978702541e21c840bc
BerendPronk <berendpronk@quicknet.nl>
2017-06-16 15:19:37 +0200

    Update menu selection for index page

--- routes/main.js
+++ routes/main.js
@@ -2,23 +2,20 @@ const debug = require('debug')('TouchCMD');
 const express = require('express');
 const router = express.Router();
 
-// [GET] /index
+// [GET] index
 router.get('/', (req, res) => {
   debug(`[${ req.method }] /index`);
 
   req.getConnection((err, connection) => {
+    // Select menu with name 'Index' from database
     connection.query(`
-      SELECT * FROM pages
-    `, [], (err, results) => {
-      let menu = [];
-
-      results.forEach(page => {
-        if (page.type === 'year') {
-          menu.push(page.title);
-        }
-      });
+      SELECT * FROM menus
+      WHERE name = 'Index'
+    `, [], (err, menus) => {
+      const menu = menus[0].children.split(',');
 
       res.render('index', {
+        tv: req.session.tv,
         menu: menu
       });
     });
