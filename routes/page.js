4f9de9012364ce88e8bc02c397c4bf73c1532095
BerendPronk <berendpronk@quicknet.nl>
2017-06-19 15:21:45 +0200

    Initialize page router

---
+++ routes/page.js
@@ -0,0 +1,134 @@
+const debug = require('debug')('TouchCMD');
+const express = require('express');
+const router = express.Router();
+
+// [GET] /page/:page
+router.get('/:page', (req, res) => {
+  debug(`[${ req.method }] /${ req.params.page }`);
+
+  // Convert
+  const title = req.params.page.replace(/-/g, ' ');
+
+  req.getConnection((err, connection) => {
+    // Select page with title from GET parameter
+    connection.query(`
+      SELECT * FROM pages
+      WHERE title = '${ title }'
+    `, [], (err, pages) => {
+      const page = pages[0];
+
+      // Page menu specific variables
+      const pageMenus = page.menus.split(',');
+      let menuChildren = [];
+
+      // Will store ordered HTML content
+      let contentBlocks = [];
+
+      // Process page content to HTML
+      page.content.split('|-|').forEach(block => {
+        console.log(block);
+
+        switch (block.charAt(1)) {
+          case 'H':
+            contentBlocks.push(`
+              <h3>${ block.replace('|H|', '') }</h3>
+            `);
+          break;
+          case 'P':
+            contentBlocks.push(`
+              <p>${ block.replace('|P|', '') }</p>
+            `);
+          break;
+          case 'I':
+            contentBlocks.push(`
+              <img src="/media/${ block.replace('|I|', '') }" alt="Image about ${ page.title }">
+            `);
+          break;
+          case 'L':
+            const listContent = block.replace('|L|', '').split('|');
+            const listName = listContent[0];
+            const list = listContent[1].split(',');
+            let listString = '';
+
+            // Give HTML to each item in list
+            list.forEach(item => {
+              listString += `
+                <li>
+                  ${ item }
+                </li>
+              `;
+            });
+
+            contentBlocks.push(`
+              <h4>${ listName }</h4>
+              <ul>
+                ${ listString }
+              </ul>
+            `);
+          break;
+          case 'E':
+            const host = block.replace('|E|', '');
+
+            // Check source of embedded video
+            if (host.indexOf('youtube.com') !== -1) {
+              contentBlocks.push(`
+                <iframe width="640" height="360" src="https://www.youtube.com/embed/${ host.split('.com/watch?v=')[1] }" frameborder="0" allowfullscreen></iframe>
+              `);
+            } else if (host.indexOf('vimeo.com') !== -1) {
+              contentBlocks.push(`
+                <iframe width="640" height="360" src="https://player.vimeo.com/video/${ host.split('.com/')[1].replace('/', '') }" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+              `);
+            }
+          break;
+          case 'B':
+            const buttonContent = block.replace('|B|', '').split('|');
+            const buttonName = buttonContent[0];
+            const buttonAnchor = buttonContent[1].split('-')[1];
+
+            contentBlocks.push(`
+              <a href="/page/${ buttonAnchor.replace(/ /g, '-').toLowerCase() }" class="button">${ buttonName }</a>
+            `);
+          break;
+        }
+      });
+
+      console.log(contentBlocks)
+
+      // Fetch all menus from database
+      connection.query(`
+        SELECT * FROM menus
+      `, [], (err, menus) => {
+
+        // Pushes array of each page menu's children
+        pageMenus.forEach(pageMenu => {
+          menus.forEach(menu => {
+            if (menu.name === pageMenu) {
+              menuChildren.push(menu.children.split(','));
+            }
+          });
+        });
+
+        // Render different view based on page type
+        switch (page.type) {
+          default:
+            // Render page view
+            res.render('page', {
+              tv: req.session.tv,
+              page: {
+                type: page.type.replace(/ /g, '-'),
+                title: page.title,
+                menus: pageMenus.filter(e => {
+                  // Removes empty data fields
+                  return e;
+                }),
+                menuChildren: menuChildren,
+                content: contentBlocks
+              }
+            });
+        }
+      });
+    });
+  });
+});
+
+module.exports = router;
