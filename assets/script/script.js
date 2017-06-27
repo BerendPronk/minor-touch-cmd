199aa8767f54c290ffd69767d56078bdc1f99531
BerendPronk <berendpronk@quicknet.nl>
2017-06-27 23:23:23 +0200

    Add modal script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,26 @@
+// Methods for opening and closing modals
+const modal = {
+  open: () => {
+    const box = document.querySelector('section[data-modal="box"]');
+    const image = box.querySelector('img');
+
+    // Set clicked image as modal image
+    image.src = event.target.src;
+
+    box.classList.remove('hidden');
+  },
+  close: () => {
+    const box = document.querySelector('section[data-modal="box"]');
+
+    box.classList.add('hidden');
+  }
+};
+
 // Service worker registration
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
+if (navigator.serviceWorker) {
+  window.addEventListener('load', function() {
     navigator.serviceWorker.register('/sw.js')
-      .then(registration => console.info('ServiceWorker registration succesful'))
-      .catch(err => console.error('ServiceWorker registration failed: ', err));
+      .then(registration => console.info('Service worker registration succesful'))
+      .catch(err => console.error('Service worker registration failed: ', err));
   });
 }
